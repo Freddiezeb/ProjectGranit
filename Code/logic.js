49165004a17113776720e394c24ccf7cd418aea6
maxhuldin <maxhuldin@gmail.com>
2018-04-28 20:18:24 +0200

    Sorting ground work, Max and Filip

--- Code/logic.js
+++ Code/logic.js
@@ -137,5 +137,100 @@ var data = {
 
     function onMessage(message) {
       data.messages.push(message.payloadString);
-      console.log("message received: " + message.payloadString);
+      //console.log("message received: " + message.payloadString);
+     
+    
+        OrderData(message)
     }
+
+
+    function OrderData(message)
+    {
+        var result = JSON.parse(message.payloadString);
+    CallFunction(result.uid, result)
+    
+    }
+    
+    function CallFunction(functionName, functionArgument)
+    {
+        var func = window[functionName](functionArgument)
+        if(typeof func === "function")
+            {
+                func.apply(window, functionArgument)
+            }
+        else
+        {
+            return
+        } 
+    }
+    
+    function arduino_due_1(result)
+    {
+        console.log("arduino_due_1")
+        
+    }
+
+    function hue_1(result)
+    {
+        console.log("hue_1")
+        
+    }
+
+    function blinds_1(result)
+    {
+        console.log("blinds_1")
+        
+    }
+    function person_count(result)
+    {
+        console.log("person_count")
+        
+    }
+    function lab_state(result)
+    {
+        console.log("lab_state")
+        
+    }
+
+     function phone_1(result)
+    {
+        console.log("phone_1")
+        
+    }
+
+    function axis_old_camera(result)
+    {
+        console.log("axis_old_camera")
+    }
+
+     function sensmitter_1(result)
+    {
+        console.log("sensmitter_1")
+    }
+    
+     function sensmitter_2(result)
+    {
+        console.log("sensmitter_2")
+    }
+    
+     function sensmitter_3(result)
+    {
+        console.log("sensmitter_3")
+    }
+
+     function cameraACCC8E7E6E9F(result)
+    {
+        console.log("cameraACCC8E7E6E9F")
+    }
+
+     function eye_contact_1(result)
+    {
+        console.log("eye_contact_1")
+    }
+
+     function user_feedback(result)
+    {
+        console.log("user_feedback")
+    }
+   
+   
