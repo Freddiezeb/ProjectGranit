850c306b92b856afcf32e0372eebfa6848d95184
davez94 <38530281+davez94@users.noreply.github.com>
2018-06-04 18:17:20 +0200

    Test

--- js/loadicon.js
+++ js/loadicon.js
@@ -16,17 +16,40 @@ var bar = new ldBar(loadBubble, {
  //Fyll bubbla färg, färg för mindre bubblor, antal bubblor i mönster, hastighet
  "fill":"data:ldbar/res,bubble(#a0d7e3,#fff,40,5)",
  //Fyllnads tid 5 sec
- "duration":"1"
+ "duration":"10"
 });
 //fyll till hundra
-bar.set(100)
+bar.set(100
+)
+//Ska in i if sats
+/*var bar = new ldBar(loadBubble, {
+// storlek på de små bubblorna
+ "pattern-size": "40",
+// Bakgrunds färg av bubbla
+ "fill-background": "transparent",
+ // preset Form: bubbla
+ "preset": "bubble",
+ // laddningstyp: fyll bubbla
+ "type":"fill",
+ //Fyll bubbla färg, färg för mindre bubblor, antal bubblor i mönster, hastighet
+ "fill":"data:ldbar/res,bubble(#a0d7e3,#fff,40,5)",
+ //Fyllnads tid 5 sec
+ "duration":"1"
+});*/
+//Ska in i if sats
 displayCharts();
 
 function displayCharts() {
+
   setTimeout(function(){
     wrapp.classList.replace("wrapper_display_none","wrapper_display");
     loadBubble.remove();
     headline.remove();
- }, 0);
 
+  }, 1000);
 }
+//if charts got data
+//if (){
+
+  //console.log("hello");
+//}
